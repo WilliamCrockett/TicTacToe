52df233545dc9d394e3f6be7a10272d615f6605e
WilliamCrockett <william@sailing.co.za>
2018-02-08 14:52:09 -0500

    Changing back to dev api
    SOmething is wrong with prod API...

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -5,7 +5,7 @@ const store = require('../store.js')
 
 const signUp = function (data) {
   return $.ajax({
-    url: 'https://ga-wdi-boston.herokuapp.com' + '/sign-up',
+    url: 'http://tic-tac-toe.wdibos.com' + '/sign-up',
     method: 'POST',
     headers: {
       contentType: 'application/json'
